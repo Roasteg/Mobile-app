a93c635ef793e3769fb90df608b87e9a7ff6d202
Opilane TTHK <opilane@tthk.ee>
2020-09-02 15:42:43 +0300

    Add project files.

---
+++ Konovalov_App/Konovalov_App/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Konovalov_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        int clik = 0;
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            //btn.Text = "Tere!";
+            clik++;
+            btn.Text = clik.ToString();
+            if (clik % 5 == 0)
+            {
+                lbl.Text = "";
+            }
+            else
+            {
+                lbl.IsVisible = true;
+                lbl.Text = "Nupp";
+            }
+        }
+    }
+}
